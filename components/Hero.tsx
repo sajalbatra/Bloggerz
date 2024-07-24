3648b07824f0b7fd74da6fdc2283c646900a2953
Sajal Batra <sajalbatra7@gmail.com>
2024-07-24 15:31:52 +0530

    some changes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -44,7 +44,7 @@ const Hero = () => {
 
     return (
         <div className="container mx-auto px-4 py-8">
-            <h1 className="text-3xl font-bold mb-4 text-center">All Blogs</h1>
+            <h1 className="text-3xl font-bold mb-4 text-center">All Blogs Here.</h1>
             {blogs.length > 0 ? (
                 <div className="grid gap-6 lg:grid-cols-2">
                     {blogs.map((blog) => (
